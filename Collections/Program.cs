2d3f625fffc1d1a82889c753cb9e3eac355102dc
Indap Pun Magar <punindap@gmail.com>
2019-05-27 21:52:22 -0500

    working with collections and arrays c#

---
+++ Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wenesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            // Console.WriteLine("Which day do you want to display?");
+            // Console.WriteLine("(Note: 0-indexed array) > ");
+            // int iDay = int.Parse(Console.ReadLine());
+
+            // string chosenDay = daysOfWeek[iDay-1];
+            // Console.WriteLine($"That day is {chosenDay}");
+
+            Console.WriteLine("Before:");
+            foreach(string day in daysOfWeek)
+                Console.WriteLine(day);
+            
+            daysOfWeek[2] = "Wednesday";
+
+            Console.WriteLine("\r\nAfter:");
+            foreach(string day in daysOfWeek)
+                Console.WriteLine(day);
+        }
+    }
+}
